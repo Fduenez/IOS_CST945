f9b9dd675e53de977358c900b4a306496f56e00e
fduenez <fduenez@ymail.com>
2019-09-23 22:09:05 -0700

    added changes

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Frank Duenez on 9/22/19.
+//  Copyright © 2019 Frank Duenez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
